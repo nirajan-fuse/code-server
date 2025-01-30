d39cd0da65d4fe1bc5303b4c130db2544f505b31
nirajan <nirajan.thakuri@fusemachines.com>
2025-01-30 15:15:26 +0545

    changed password and LAB_USER to token and NOTEBOOK_USER respectively

--- .code-server-build/start-nginx.sh
+++ .code-server-build/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-LAB_USER=${LAB_USER}
-sudo sed -i "s|\${LAB_USER}|${LAB_USER}|g" /tmp/login-with-args.html
+NOTEBOOK_USER=${NOTEBOOK_USER}
+sudo sed -i "s|\${NOTEBOOK_USER}|${NOTEBOOK_USER}|g" /tmp/login-with-args.html
 
 sh /tmp/20-envsubst-on-templates.sh
 
